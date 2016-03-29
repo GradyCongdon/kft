cb97b7480c81cff2ae2fd193e9137cfd0fb74cf0
gradycongdon <gradycongdon@gmail.com>
2016-03-29 01:56:48 +0000

    Adds session, assets served by nginx, layout template, css

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, url_for, render_template, request, redirect
+from flask import Flask, url_for, render_template, request, redirect, session
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/home/ec2-user/www/printer/uploads/'
@@ -7,13 +7,18 @@ ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key ='\xf0e\x07\nT\x9e\x92\x16uWh\xb1\xb9\x8f\xca\xb0\xe3\xd2@\xef\x1e\x03\xb1M'
 
 @app.route('/')
 def hello():
-    return "heyyyyy"
+    if 'user' in session:
+        return "heyyyyy %s" % session['user']
+    else:
+        return "hi"
 
 @app.route('/user/<name>')
 def greet(name):
+    session['user'] = name
     return render_template('greet.html', name=name)
 
 @app.route('/upload', methods=['GET', 'POST'])
