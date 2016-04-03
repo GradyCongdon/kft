21d4e210739769fe860e2addd7a07ef9dc33f381
gradycongdon <gradycongdon@gmail.com>
2016-04-03 13:49:26 +0000

    Adds ES6 gulpfiles & scss/js tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import jshint from 'gulp-jshint'
@@ -6,17 +8,39 @@ import sass from 'gulp-sass'
 import concat from 'gulp-concat'
 import uglify from 'gulp-uglify'
 import rename from 'gulp-rename'
+import autoprefixer from 'gulp-autoprefixer'
 
 const dirs = {
     src: 'src',
     dest: 'assets'
 };
 
-gulp.task('sass', () => {
-  return gulp.src(dirs.src)
+const files = {
+    scss: '*.scss',
+    js: '*.js'
+};
+
+gulp.task('default', ['js', 'sass'], () => {
+});
+
+gulp.task('test', () => {
+  console.log('ok!')
+});
+
+gulp.task('js', () => {
+  return gulp.src(dirs.src+files.js)
     .pipe(sourcemaps.init())
-    .pipe(sass.sync().on('error', plugins.sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('.'))
+    .pipe(babel({presets:['es2015']}))
+    .pipe(sourcemaps.write(dirs.dest))
     .pipe(gulp.dest(dirs.dest));
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./src/scss.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename('css.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./assets'))
 });
